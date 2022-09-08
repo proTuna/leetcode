eeece8fca92079ad3c7e8786493d2a002758e381
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-08 15:27:12 +0900

    Time: 757 ms (21.06%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -2,6 +2,7 @@
 # select E1.name as Employee
 # from Employee as E1, Employee as E2
 # where E1.managerId = E2.id and E1.salary > E2.salary
+# 셀프 조인이 더 느리다
 select E1.name as Employee
 from Employee as E1
 join Employee as E2 on E1.managerId = E2.Id
