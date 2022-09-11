50dc979d33e25be3f22b48b33aef7d22135869ac
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-11 23:49:25 +0900

    Time: 150 ms (7.65%), Space: 15.1 MB (69.40%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        s = []
+        ans = 0
+        for i in prices:
+            if not s:
+                s.append(i)
+                continue
+            if s[-1] > i:
+                ans += max(s)-min(s)
+                while s:
+                    s.pop()
+                s.append(i)
+            else:
+                s.append(i)
+                
+        if s:
+            ans += s[-1] - s[0]
+        return ans
