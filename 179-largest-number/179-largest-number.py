07413488895cc85c5852bd14b1ef6840e639e736
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-12 18:41:55 +0900

    Time: 140 ms (7.16%), Space: 13.8 MB (67.91%) - LeetHub

--- 179-largest-number/179-largest-number.py
+++ 179-largest-number/179-largest-number.py
@@ -1,5 +1,17 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        numbers = list(map(str, nums))
-        numbers.sort(key=lambda x: x*9, reverse=True)
-        return str(int(''.join(numbers)))
+        # numbers = list(map(str, nums))
+        # numbers.sort(key=lambda x: x*9, reverse=True)
+        # return str(int(''.join(numbers)))
+        def swap(n1: int, n2: int) -> bool:
+            return str(n1) + str(n2) < str(n2) + str(n1)
+        
+        i = 1
+        while i < len(nums):
+            j = i
+            while j>0 and swap(nums[j-1], nums[j]):
+                nums[j], nums[j-1] = nums[j-1], nums[j]
+                j-=1
+            i+=1
+            
+        return str(int(''.join(map(str,nums))))
