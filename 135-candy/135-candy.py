a071b245f5ee803090ac93ed790ad05e0790c7a1
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-12 18:29:33 +0900

    Time: 356 ms (19.47%), Space: 16.8 MB (76.53%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,14 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        l = len(ratings)
+        candies = [1 for _ in range(l)]
+        for i in range(1,l):
+            if ratings[i]>ratings[i-1]:
+                candies[i] = candies[i-1]+1
+        print(candies)
+        for i in range(l-1,0,-1):
+            if ratings[i-1] > ratings[i]:
+                candies[i-1] = max(candies[i-1],candies[i]+1)
+
+        return sum(candies)
+            
