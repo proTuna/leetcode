80a259a52e186ffbb9c2f04d51bd387929672cc7
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-12 19:52:35 +0900

    Time: 61 ms (47.98%), Space: 14 MB (9.29%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.py
@@ -8,6 +8,10 @@ class Solution:
         
         for i in range(len(s)):
             if s[i] not in stack:
+                # 스택이 비어있지 않고,
+                # 스택의 top이 현재 넣으려는 값보다 크며,
+                # 스택의 top이 미래에 다시 사용될 것이라면 
+                # 스택에서 pop을 한다.
                 while (stack and stack[-1] >= s[i] and last_occ[stack[-1]] >= i):
                     stack.pop()
                 stack.append(s[i])
