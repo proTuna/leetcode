7fc543dd22f66ff24f8a429ed96867b5a6b44f51
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-14 13:41:34 +0900

    Time: 2030 ms (11.84%), Space: 86.1 MB (27.10%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -7,25 +7,35 @@
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
         
+        # 2번째 풀이
+        target = {0,1,2,4,8,16,32,64,128,256} # 1-9까지 이므로
+        def dfs(node,s):
+            s ^=1 << (node.val-1)
+            if not node.left and not node.right: # pseudo-palindromic 하려면 odd 숫자는 1개이하여야한다.
+                return int(s in target)
+            return (dfs(node.left,s) if node.left else 0) + (dfs(node.right,s) if node.right else 0)
+        return dfs(root, 0)
+        
         # In the leaf, if the set is empty, then its an even palindrome.
         # In the leaf, if the set has 1 element , its an odd palindrome.
         # In th leaf, if the set has > 1 elements, its not a palindrome.
-        
-        def dfs(node, s):
-            if not node:
-                return 0
+# 1번째 풀이      
+#         def dfs(node, s):
+#             if not node:
+#                 return 0
             
-            if node.val in s:
-                s.remove(node.val)
-            else:
-                s.add(node.val)
+#             if node.val in s:
+#                 s.remove(node.val)
+#             else:
+#                 s.add(node.val)
                 
-            if not node.left and not node.right:
-                return 1 if len(s) <=1 else 0
+#             if not node.left and not node.right:
+#                 return 1 if len(s) <=1 else 0
             
-            left = dfs(node.left, set(s))
-            right = dfs(node.right, set(s))
+#             left = dfs(node.left, set(s))
+#             right = dfs(node.right, set(s))
             
-            return left+right
+#             return left+right
         
-        return dfs(root,set())
+#         return dfs(root,set())
+            
