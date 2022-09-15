107248fc4f3e19ede4554e002b34215351b6309e
proTuna <76765504+proTuna@users.noreply.github.com>
2022-09-15 18:54:41 +0900

    Time: 89 ms (48.23%), Space: 14 MB (58.35%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -2,9 +2,6 @@ import heapq as hq
 
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        # 음수인 경우 k 사용
-        # 양수인 경우 k 2번 사용
-        # 음수가 없고 0이 있다면 k 다사용
         hq.heapify(nums)
         while k:
             num = hq.heappop(nums)
